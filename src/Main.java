3abb75618136324831d0e7aa1b699d7de32deb8c
Elias <elias.bs2@hotmail.com>
2022-11-09 16:47:10 +0100

    creo un proyecto limpio con IntelliJ

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    //warning: in the working copy of 'src/Main.java', LF will be replaced by CRLF the next time Git touches it
+
+    // lf = caracteres de salto de linea
+    // crlf = CRLF se refiere a la combinación de dos códigos de control: CR (retorno de carro) y LF (salto de línea),
+
+
+
+
 }
